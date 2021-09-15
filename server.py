fd1687283f1f3390c514f363ef94ad3ac1a7536c
Nate Nelson <nathan.nelson99@gmail.com>
2021-09-14 23:29:12 -0600

    Adds first few requirements for project

---
+++ server.py
@@ -0,0 +1,83 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+    <a href="http://localhost:5000/hello">Hi! This is the home page.</a>
+    </html>
+    """
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <input type="radio" id="comp_one" name="comp_choice" value="awesome">
+          <label for="comp_one">Awesome</label><br>
+          <input type="radio" id="comp_two" name="comp_choice" value="lovely">
+          <label for="comp_two">Lovely</label><br>
+          <input type="radio" id="comp_three" name="comp_choice" value="brilliant">
+          <label for="comp_three">Brilliant</label><br>
+          <input type="radio" id="comp_four" name="comp_choice" value="cool">
+          <label for="comp_four">Cool</label><br>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+
+    compliment = request.args.get("comp_choice")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
